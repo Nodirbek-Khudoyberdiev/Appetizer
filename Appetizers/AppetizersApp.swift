2de67b8b90569fa85923ff200567eaa12dee59e8
Nodirbek Khudoyberdiev <kh.nodirbeek@gmail.com>
2023-08-31 22:41:19 +0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Nodirbek Khudoyberdiev on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
